0ef1166f6f53d70ebc08b7a3f9a97241a8fc3103
ThomasLC-Dev <thomas.le-corvec@outlook.com>
2025-06-06 16:47:29 +0200

    Add Logout button

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { ButtonComponent } from '../../components/button/button.component';
 import { StorageService } from '../../utils/storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,8 +11,10 @@ import { StorageService } from '../../utils/storage.service';
 })
 export class DashboardComponent {
     storageService = inject(StorageService);
+    router = inject(Router);
 
     logout() {
         this.storageService.clearToken();
+        this.router.navigate(["login"]);
     }
 }
